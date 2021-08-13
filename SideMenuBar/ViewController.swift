8b55ac321ec16ee9631eae014edaec0b6f6163bc
Harsh  Verma <bl00d_9@icloud.com>
2021-08-13 17:52:26 +0530

    Initial Commit

---
+++ SideMenuBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideMenuBar
+//
+//  Created by Harsh Verma on 13/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
